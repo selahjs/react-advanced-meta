3300246b21e09bedd103ed38fe4d46f0809cc3b6
Selahadin Jemal <sjemal99@gmail.com>
2024-06-22 15:28:30 +0300

    using render props to share logic between components and enable reusability

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,78 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-//  I used "extends React.ButtonHTMLAttributes<HTMLButtonElement>" so that any available button attributes can be passed
-// without type issue
-interface IParams extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: any;
-  socials?: any;
+interface IParams {
+  render: any;
+  url?: any;
 }
 
-const withSocialsHOC = (WrappedComponent, display=false) => {
-  return (props) => {
-    const [social, setSocial] = useState({
-      facebook: "FB",
-      instagram: "IG",
-    });
-    return (
-      <>
-        {/* we can pass any props or cuntionality to be used by the component the way they like */}
-        <WrappedComponent socials={social} {...props} />
-        {/* or we can just wrap whatever we want around the component */}
-        {display && <h3><a href="https://facebook.com" target="_blank">fb</a></h3>}
-      </>
-    );
-  };
+// the component who is going to use the render prop / data fetcher can choose how to display/render the data
+// we can easiliy reuse the component 
+const DataFetcher = ({ render, url }: IParams) => {
+  const [data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    if (url.includes("people")) {
+      setData(["Mubarek", "Redwan", "Selahadin"]);
+    } else {
+      setData(["React", "Angular"]);
+    }
+  }, []);
+
+  return render(data);
 };
-const Home = ({ socials }: IParams) => {
+
+const People = () => {
   return (
     <>
-      <h1>Home</h1>
-      <p>This is a home</p>
-      <p>
-        {socials.facebook}, {socials.instagram}
-      </p>
+      <DataFetcher
+        url={"http://example.com/people"}
+        render={(data: any) => {
+          if (!data.length) {
+            return <p>loading...</p>;
+          }
+          return (
+            <div style={{ border: 'solid'}}>
+              <p>I'm using this component to list the data</p>
+              <hr />
+              <h2>People</h2>
+              <p>There are {data.length} people, which are:</p>
+              <ul>
+                {data.map((d) => (
+                  <li>{d}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        }}
+      />
     </>
   );
 };
-const About = () => {
+const Languages = () => {
   return (
-    <>
-      <h1>About</h1>
-      <p>This is about with a link</p>
-    </>
+    <div style={{ border: 'solid', marginTop: 1}}>
+      <DataFetcher
+        url={"http://example.com/languages"}
+        render={(data: any) => (
+          <div>
+            <p>I'm using this component to show number of data</p>
+            <hr />
+            <p>there are {data.length} frontend libraries</p>
+          </div>
+        )}
+      />
+    </div>
   );
 };
-const HomeWithSocials = withSocialsHOC(Home);
-const AboutWithSocials = withSocialsHOC(About, true);
+
 function App() {
   return (
-    <>
-      <header>Little Lemon</header>
-      <HomeWithSocials />
-      <AboutWithSocials />
-    </>
+    <div>
+      <header>Using render props</header>
+      <hr />
+      <People />
+      <Languages />
+    </div>
   );
 }
 
---
+++ src/ReactHOC.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./App.css";
+//  I used "extends React.ButtonHTMLAttributes<HTMLButtonElement>" so that any available button attributes can be passed
+// without type issue
+interface IParams extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: any;
+  socials?: any;
+}
+
+const withSocialsHOC = (WrappedComponent:any, display=false) => {
+  return (props: any) => {
+    const [social] = useState({
+      facebook: "FB",
+      instagram: "IG",
+    });
+    return (
+      <>
+        {/* we can pass any props or cuntionality to be used by the component the way they like */}
+        <WrappedComponent socials={social} {...props} />
+        {/* or we can just wrap whatever we want around the component */}
+        {display && <h3><a href="https://facebook.com" target="_blank">fb</a></h3>}
+      </>
+    );
+  };
+};
+const Home = ({ socials }: IParams) => {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>This is a home</p>
+      <p>
+        {socials.facebook}, {socials.instagram}
+      </p>
+    </>
+  );
+};
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is about with a link</p>
+    </>
+  );
+};
+const HomeWithSocials = withSocialsHOC(Home);
+const AboutWithSocials = withSocialsHOC(About, true);
+function App() {
+  return (
+    <>
+      <header>Little Lemon</header>
+      <HomeWithSocials />
+      <AboutWithSocials />
+    </>
+  );
+}
+
+export default App;
